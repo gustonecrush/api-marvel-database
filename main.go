5ca7f54e6d502e95120a478add4095688a51571e
gustonecrush <codeboxezz@gmail.com>
2023-02-03 02:01:44 +0700

    setted up endpoints

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gustonecrush/api-marvel-database/models"
+)
+
 func main() {
-	
+
+	r := gin.Default()
+	models.ConnectDatabase()
+
+	// routes for API
+	r.GET("/api/heroes", herocontroller.Index)
+	r.GET("/api/heroes/:id", herocontroller.Show)
+	r.POST("/api/heroes", herocontroller.Create)
+	r.PUT("/api/heroes/:id", herocontroller.Update)
+	r.DELETE("/api/heroes", herocontroller.Delete)
+
+	r.Run()
+
 }
