be9d76e5696902d4e4e332b1564f4929c60b2c57
gustonecrush <codeboxezz@gmail.com>
2023-02-03 19:00:41 +0700

    installed jwt golang and mux also initialized router

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/mux"
 	"github.com/gustonecrush/api-marvel-database/models"
 	"github.com/gustonecrush/api-marvel-database/controllers/herocontroller"
+	"github.com/gustonecrush/api-marvel-database/controllers/authcontroller"
 )
 
 func main() {
@@ -11,12 +13,21 @@ func main() {
 	r := gin.Default()
 	models.ConnectDatabase()
 
-	// routes for API
+	// endpoints hero
 	r.GET("/api/heroes", herocontroller.Index)
 	r.GET("/api/heroes/:id", herocontroller.Show)
 	r.POST("/api/heroes", herocontroller.Create)
 	r.PUT("/api/heroes/:id", herocontroller.Update)
 	r.DELETE("/api/heroes", herocontroller.Delete)
+
+	// endpoints auth
+
+	auth := mux.NewRouter()
+
+	auth.HandleFunc("/api/login", authcontroller.Login).Methods("POST")
+	auth.HandleFunc("/api/register", authcontroller.Register).Methods("POST")
+	auth.HandleFunc("/api/logout", authcontroller.Logout).Methods("POST")
+
 
 	r.Run()
 
